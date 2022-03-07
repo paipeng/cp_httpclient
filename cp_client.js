63221e518c7f8d21c2cf9144686e9ace029b56d3
Pai Peng <sipaipv6@gmail.com>
2022-03-07 17:31:27 +0800

    use class instead

--- cp_client.js
+++ cp_client.js
@@ -5,29 +5,34 @@ function CpClient() {
 
 }
 
-CpClient.prototype.decode = function (detect, success, fail) {
-    https.get(this.serverUrl, (resp) => {
-        let data = '';
+class CPClient {
+    constructor(serverUrl) {
+        this.serverUrl = serverUrl;
 
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            data += chunk;
+    }
+    decode(detect, success, fail) {
+        https.get(this.serverUrl, (resp) => {
+            let data = '';
+
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                console.log('end');
+                //console.log(JSON.parse(data).explanation);
+                success(JSON.parse(data).explanation);
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            //fail(err);
         });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log('end');
-            //console.log(JSON.parse(data).explanation);
-            success(JSON.parse(data).explanation);
-        });
-
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-        //fail(err);
-    });
-};
+    }
+}
 
 
-module.exports = {
-    cpClient: new CpClient()
-};
+module.exports = CPClient;
+
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-var httpClient = require('./cp_client');
-
+var CPClient = require('./cp_client');
 var CPDetect = require('./cp_detect');
 var CPResult = require('./cp_result');
 
 const detect = new CPDetect(10, 10);
 console.log(detect);
 
-httpClient.cpClient.decode(0, function (response) {
+var httpClient = new CPClient('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+
+httpClient.decode(0, function (response) {
     console.log(response);
 }, function (error) {
     console.error(error);
